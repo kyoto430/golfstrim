84a4ca6c9551c72a165d48d0a5a5473d9779acbc
Ruslan <nateriver345@gmail.com>
2024-08-28 22:34:13 +0300

    add: sections

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -53,3 +53,57 @@ new Swiper('.organization__slider', {
     },
   },
 });
+
+new Swiper('.agency__slider', {
+  spaceBetween: 20,
+  loop: false,
+  navigation: {
+    prevEl: '.agency__p-1',
+    nextEl: '.agency__n-1',
+  },
+  pagination: {
+    el: '.agency__p',
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    744: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+new Swiper('.clients__slider', {
+  spaceBetween: 20,
+  loop: false,
+  navigation: {
+    prevEl: '.clients__p-1',
+    nextEl: '.clients__n-1',
+  },
+  pagination: {
+    el: '.clients__p',
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    744: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
